7ced8b8fb7cb04b9dc04c6ceae8f225f1dc817fb
Mai Heaven <longvt>
2021-10-06 10:53:16 +0700

    how2authen

--- frontend/src/component/Section/Show.js
+++ frontend/src/component/Section/Show.js
@@ -3,17 +3,12 @@ import React, { Component } from "react";
 class Show extends Component {
   render() {
     return (
-      // <img
-      //   src={this.props.selectImg.link}
-      //   id={this.props.selectImg.id}
-      //   alt=""
-      //   className="large-preview"
-      // />
-      <iframe
-        src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTFzyBwZPlRHFiLLrvxBssLVT18FIbr0itRd86a6Lj1__7MQyRfZxXpT8yAWzJe5MJJXVj1ieoO4gcT/pubhtml?widget=true&amp;headers=false"
-        style={{ height: "100%", width: "100%" }}
-        headers="true"
-      ></iframe>
+      <img
+        src={this.props.selectImg.link}
+        id={this.props.selectImg.id}
+        alt=""
+        className="large-preview"
+      />
     );
   }
 }
